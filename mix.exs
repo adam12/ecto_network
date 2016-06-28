ea3265d42f4e9da5d3cc1871c11ba8e6c549284c
Adam Daniels <adam@mediadrive.ca>
2016-06-27 21:49:00 -0400

    Update mix.exs for Hex

--- mix.exs
+++ mix.exs
@@ -7,28 +7,32 @@ defmodule EctoNetwork.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps(),
+     description: description(),
+     package: package()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [{:ecto, ">= 0.0.0"},
      {:postgrex, ">= 0.0.0"},
      {:phoenix_html, ">= 0.0.0", [optional: true]}]
+  end
+
+  defp description do
+    """
+    Ecto types to support MACADDR and Network extensions provided by Postgrex.
+    """
+  end
+
+  defp package do
+    [
+      maintainers: ["Adam Daniels"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/adam12/ecto_network"}
+    ]
   end
 end
