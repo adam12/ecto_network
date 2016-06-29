ad56a14404b2494b872e51e534a1fe4d6f3651b1
Adam Daniels <adam@mediadrive.ca>
2016-06-28 23:33:08 -0400

    Remove commented types [ci skip]

--- test/ecto_migration.exs
+++ test/ecto_migration.exs
@@ -3,9 +3,9 @@ defmodule Ecto.Integration.Migration do
 
   def change do
     create table(:devices) do
-      add :macaddr, :macaddr #EctoNetwork.MACADDR
-      add :ip_address, :inet #EctoNetwork.INET
-      add :network, :cidr #EctoNetwork.CIDR
+      add :macaddr, :macaddr
+      add :ip_address, :inet
+      add :network, :cidr
     end
   end
 end
