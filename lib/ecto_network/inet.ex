fe1471e9ab9772480e0a6222b99cea8ad134d9b6
Adam Daniels <adam@mediadrive.ca>
2016-09-17 17:23:22 -0400

    Add moduledoc/doc strings for all modules and functions

--- lib/ecto_network/inet.ex
+++ lib/ecto_network/inet.ex
@@ -1,8 +1,13 @@
 defmodule EctoNetwork.INET do
+  @moduledoc ~S"""
+  Support for using Ecto with :inet fields
+  """
+
   @behaviour Ecto.Type
 
   def type, do: :inet
 
+  @doc "Handle casting to Postgrex.INET"
   def cast(%Postgrex.INET{}=address), do: {:ok, address}
   def cast(address) when is_binary(address) do
     case parse_address(address) do
@@ -12,12 +17,15 @@ defmodule EctoNetwork.INET do
   end
   def cast(_), do: :error
 
+  @doc "Load from the native Ecto representation"
   def load(%Postgrex.INET{}=address), do: {:ok, address}
   def load(_), do: :error
 
+  @doc "Convert to the native Ecto representation"
   def dump(%Postgrex.INET{}=address), do: {:ok, address}
   def dump(_), do: :error
 
+  @doc "Convert from native Ecto representation to a binary"
   def decode(%Postgrex.INET{address: address}) do
     case :inet.ntoa(address) do
       {:error, _einval} -> :error
--- lib/ecto_network/macaddr.ex
+++ lib/ecto_network/macaddr.ex
@@ -1,8 +1,13 @@
 defmodule EctoNetwork.MACADDR do
+  @moduledoc ~S"""
+  Support for using Ecto with :macaddr fields
+  """
+
   @behaviour Ecto.Type
 
   def type, do: :macaddr
 
+  @doc "Handle casting to Postgrex.MACADDR"
   def cast(%Postgrex.MACADDR{}=address), do: {:ok, address}
   def cast(address) when is_binary(address) do
     [a, b, c, d, e, f] =
@@ -13,12 +18,15 @@ defmodule EctoNetwork.MACADDR do
     {:ok, %Postgrex.MACADDR{address: {a, b, c, d, e, f}}}
   end
 
+  @doc "Load from the native Ecto representation"
   def load(%Postgrex.MACADDR{}=address), do: {:ok, address}
   def load(_), do: :error
 
+  @doc "Convert to the native Ecto representation"
   def dump(%Postgrex.MACADDR{}=address), do: {:ok, address}
   def dump(_), do: :error
 
+  @doc "Convert from native Ecto representation to a binary"
   def decode(%Postgrex.MACADDR{address: {a, b, c, d, e, f}}) do
     [a, b, c, d, e, f]
     |> Enum.map(&Integer.to_string(&1, 16))
