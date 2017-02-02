e4b7a5607d398c8e7120cb922d4f24f36565517a
Adam Daniels <adam@mediadrive.ca>
2017-02-01 20:20:59 -0500

    Bump Elixir to 1.3
    String.pad_leading/3 is only on 1.3

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule EctoNetwork.Mixfile do
   def project do
     [app: :ecto_network,
      version: @version,
-     elixir: "~> 1.2",
+     elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps(),
