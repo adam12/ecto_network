ce1d03fe3a9ba46c6bf0f78aa99e168b07a93a17
Adam Daniels <adam@mediadrive.ca>
2018-10-29 14:29:34 +0000

    Minimum Elixir version is now 1.5

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule EctoNetwork.Mixfile do
     [
       app: :ecto_network,
       version: @version,
-      elixir: "~> 1.3",
+      elixir: "~> 1.5",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps(),
